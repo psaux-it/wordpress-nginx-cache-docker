c84d680839fb0d49cfee7fc3d21a8ec55781f9df
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-03-15 00:51:50 +0300

    use flow.request.host

--- mitmproxy/npp_addon.py
+++ mitmproxy/npp_addon.py
@@ -1,14 +1,16 @@
 from mitmproxy import http
 
+
 class NPPAddon:
-    def http_connect(self, flow):
+    def http_connect(self, flow: http.HTTPFlow) -> None:
         # Rewrite localhost → nginx (Docker service name) for CONNECT tunneling
-        if flow.server_conn.address[0] == 'localhost':
-            flow.server_conn.address = ('nginx', flow.server_conn.address[1])
+        if flow.request.host == 'localhost':
+            flow.request.host = 'nginx'
 
     def request(self, flow: http.HTTPFlow) -> None:
         # Rewrite localhost → nginx for direct requests
         if flow.request.pretty_host == 'localhost':
             flow.request.host = 'nginx'
+
 
 addons = [NPPAddon()]
